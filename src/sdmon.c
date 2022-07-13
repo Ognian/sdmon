0b14ef1b2f70ec8678e4c8678de15cbfe8d2ac02
Ognian Tschakalov <ognian.tschakalov@ogi-it.com>
2022-07-13 15:39:56 +0200

    - added .clang-format - reformatted

--- src/sdmon.c
+++ src/sdmon.c
@@ -35,19 +35,18 @@
 #define MMC_RSP_SPI_R1 (MMC_RSP_SPI_S1)
 #define MMC_RSP_SPI_R1B (MMC_RSP_SPI_S1 | MMC_RSP_SPI_BUSY)
 
-#define MMC_RSP_NONE	(0)
+#define MMC_RSP_NONE (0)
 #define MMC_RSP_R1 (MMC_RSP_PRESENT | MMC_RSP_CRC | MMC_RSP_OPCODE)
-#define MMC_RSP_R1B                                                            \
-  (MMC_RSP_PRESENT | MMC_RSP_CRC | MMC_RSP_OPCODE | MMC_RSP_BUSY)
+#define MMC_RSP_R1B (MMC_RSP_PRESENT | MMC_RSP_CRC | MMC_RSP_OPCODE | MMC_RSP_BUSY)
 
 // these are not needed actually
 // #include "mmc.h"
 // #include "mmc_cmds.h"
 
+#include <errno.h>
 // this is the include defining the mmc_ioc_cmd struct and mmc_ioc_cmd_set_data
 // helper etc ...
 #include <linux/mmc/ioctl.h>
-#include <errno.h>
 
 #include <time.h>
 
@@ -90,9 +89,9 @@ int CMD56_write(int fd, int cmd56_arg) {
   // data_in[0]=0x10; // also cmd56_arg macht keinen Unterschied ...
 
   // idata.flags = MMC_RSP_SPI_R1 | MMC_RSP_R1 | MMC_CMD_AC | MMC_CMD_ADTC; //this works (and that matches the spec. Table 4.29 of Part1_Physical_Layer_Simplified_Specification_Ver6.00.pdf)
-  idata.flags = MMC_RSP_R1 | MMC_CMD_ADTC; //this works (and that matches the spec. Table 4.29 of Part1_Physical_Layer_Simplified_Specification_Ver6.00.pdf)
+  idata.flags = MMC_RSP_R1 | MMC_CMD_ADTC; // this works (and that matches the spec. Table 4.29 of Part1_Physical_Layer_Simplified_Specification_Ver6.00.pdf)
   // this looks better and works also
-  //idata.flags = MMC_RSP_SPI_R1B | MMC_RSP_R1B | MMC_CMD_AC;
+  // idata.flags = MMC_RSP_SPI_R1B | MMC_RSP_R1B | MMC_CMD_AC;
 
   // useless tests
   // idata.flags &= ~MMC_RSP_CRC; /* Ignore CRC */
@@ -138,8 +137,8 @@ int main(int argc, const char *argv[]) {
   unsigned char data_in[SD_BLOCK_SIZE];
   int ret;
 
-  int i=0;
-  unsigned long sum =0;
+  int i = 0;
+  unsigned long sum = 0;
 
   time_t t = time(NULL);
   struct tm tm = *localtime(&t);
@@ -173,45 +172,39 @@ int main(int argc, const char *argv[]) {
   cmd56_arg = 0x00000001;
   ret = CMD56_data_in(fd, cmd56_arg, data_in);
   // we assume success when the call was successful AND the signature is not 0xff 0xff
-  if (ret == 0 &&
-     !(
-     (data_in[0] == 0xff && data_in[1] == 0xff) ||
-     (data_in[0] == 0x00 && data_in[1] == 0x00)
-     )) {
-       printf("\"signature\":\"0x%x 0x%x\",\n",data_in[0],data_in[1]);
-       if (data_in[0] == 0x44 && data_in[1] == 0x53) {
-         printf("\"SanDisk\":\"true\",\n");
-       }
-
-     /*
-       Health Status is an estimated percent life used based on the amount of TBW the NAND
-       memory has experienced relative to the SD card device TBW ability. Values reported in
-       hexadecimal in 1% increments with 0x01 representing 0.0% to 0.99% used. A value of
-       0x64 indicates 99 to 99.99% of the ability have been used. The SD card storage device
-       may accommodate writes in excess of the 100% expected life limit. Note that although
-       this is possible, entry into a read only mode could occur upon the next write cycle.
-     */
-
-       strncpy(tmpstr, (char *)&data_in[2], 6);
-       tmpstr[6] = 0;
-       printf("\"manufactureYYMMDD\": \"%s\",\n", tmpstr);
-       printf("\"healthStatusPercentUsed\": %d,\n", data_in[8]);
-       printf("\"featureRevision\": \"0x%x\",\n", data_in[11]);
-       printf("\"generationIdentifier\": %d,\n", data_in[14]);
-       strncpy(tmpstr, (char *)&data_in[49], 32);
-       tmpstr[32] = 0;
-       printf("\"productString\": \"%s\",\n", tmpstr);
-       close(fd);
-       printf("\"success\":true\n}\n");
-       exit(0);
-
+  if (ret == 0 && !((data_in[0] == 0xff && data_in[1] == 0xff) || (data_in[0] == 0x00 && data_in[1] == 0x00))) {
+    printf("\"signature\":\"0x%x 0x%x\",\n", data_in[0], data_in[1]);
+    if (data_in[0] == 0x44 && data_in[1] == 0x53) {
+      printf("\"SanDisk\":\"true\",\n");
     }
-    if (ret == 0) {
-      printf("\"read_via_cmd56_arg_1\":\"read successful but signature 0x%x 0x%x\",\n",data_in[0],data_in[1]);
-    } else {
-      printf("\"read_via_cmd56_arg_1\":\"not implemented: %s\",\n",
-             strerror(errno));
-    }
+
+    /*
+      Health Status is an estimated percent life used based on the amount of TBW the NAND
+      memory has experienced relative to the SD card device TBW ability. Values reported in
+      hexadecimal in 1% increments with 0x01 representing 0.0% to 0.99% used. A value of
+      0x64 indicates 99 to 99.99% of the ability have been used. The SD card storage device
+      may accommodate writes in excess of the 100% expected life limit. Note that although
+      this is possible, entry into a read only mode could occur upon the next write cycle.
+    */
+
+    strncpy(tmpstr, (char *)&data_in[2], 6);
+    tmpstr[6] = 0;
+    printf("\"manufactureYYMMDD\": \"%s\",\n", tmpstr);
+    printf("\"healthStatusPercentUsed\": %d,\n", data_in[8]);
+    printf("\"featureRevision\": \"0x%x\",\n", data_in[11]);
+    printf("\"generationIdentifier\": %d,\n", data_in[14]);
+    strncpy(tmpstr, (char *)&data_in[49], 32);
+    tmpstr[32] = 0;
+    printf("\"productString\": \"%s\",\n", tmpstr);
+    close(fd);
+    printf("\"success\":true\n}\n");
+    exit(0);
+  }
+  if (ret == 0) {
+    printf("\"read_via_cmd56_arg_1\":\"read successful but signature 0x%x 0x%x\",\n", data_in[0], data_in[1]);
+  } else {
+    printf("\"read_via_cmd56_arg_1\":\"not implemented: %s\",\n", strerror(errno));
+  }
 
   // prepare for health command
   cmd56_arg = 0x00000010; // all other are 0
@@ -238,8 +231,7 @@ int main(int argc, const char *argv[]) {
   printf("\"flashId\": "
          "[\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\","
          "\"0x%02x\",\"0x%02x\",\"0x%02x\"],\n",
-         data_in[0], data_in[1], data_in[2], data_in[3], data_in[4], data_in[5],
-         data_in[6], data_in[7], data_in[8]);
+         data_in[0], data_in[1], data_in[2], data_in[3], data_in[4], data_in[5], data_in[6], data_in[7], data_in[8]);
   printf("\"icVersion\": [\"0x%02x\",\"0x%02x\"],\n", data_in[9], data_in[10]);
   printf("\"fwVersion\": [%02d,%02d],\n", data_in[11],
          data_in[12]); // show in decimal
@@ -247,7 +239,7 @@ int main(int argc, const char *argv[]) {
 
   // printf("\"badBlockReplaceMaximum\": [\"0x%02x\",\"0x%02x\"],\n", data_in[16], data_in[17]);
   // badBlockReplaceMaximum is spareBlockCount
-  printf("\"spareBlockCount\": %d,\n", (int)((data_in[16]<<8)+data_in[17]));
+  printf("\"spareBlockCount\": %d,\n", (int)((data_in[16] << 8) + data_in[17]));
 
   //  printf("\"badBlockCountPerDie1\": "
   //         "[\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\","
@@ -264,33 +256,32 @@ int main(int argc, const char *argv[]) {
   //         data_in[57], data_in[58], data_in[59], data_in[60], data_in[61],
   //         data_in[62], data_in[63]);
   // sum up to get initial bad block count
-  sum=0;
-  for(i=32; i<64; i++) sum+=data_in[i];
+  sum = 0;
+  for (i = 32; i < 64; i++)
+    sum += data_in[i];
   printf("\"initialBadBlockCount\": %ld,\n", sum);
 
   // printf("\"goodBlockRatePercentBytes\": [\"0x%02x\",\"0x%02x\"],\n", data_in[64], data_in[65]);
   // printf("\"goodBlockRatePercent\": %d,\n", (int)((data_in[64]<<8)+data_in[65]));
-  printf("\"goodBlockRatePercent\": %2.2f,\n", (float)((float)((int)((data_in[64]<<8)+data_in[65]))/100));
+  printf("\"goodBlockRatePercent\": %2.2f,\n", (float)((float)((int)((data_in[64] << 8) + data_in[65])) / 100));
 
-  printf("\"totalEraseCount\": %ld,\n", (long)((data_in[80]<<24)+(data_in[81]<<16)+(data_in[82]<<8)+data_in[83]));
+  printf("\"totalEraseCount\": %ld,\n", (long)((data_in[80] << 24) + (data_in[81] << 16) + (data_in[82] << 8) + data_in[83]));
 
   // printf("\"enduranceRemainLifePercentBytes\": [\"0x%02x\",\"0x%02x\"],\n", data_in[96], data_in[97]);
   // printf("\"enduranceRemainLifePercent\": %d,\n", (int)((data_in[96]<<8)+data_in[97]));
-  printf("\"enduranceRemainLifePercent\": %2.2f,\n", (float)((float)((int)((data_in[96]<<8)+data_in[97]))/100));
+  printf("\"enduranceRemainLifePercent\": %2.2f,\n", (float)((float)((int)((data_in[96] << 8) + data_in[97])) / 100));
 
+  printf("\"avgEraseCount\": %ld,\n", (long)((data_in[104] << 24) + (data_in[105] << 16) + (data_in[98] << 8) + data_in[99]));
+  printf("\"minEraseCount\": %ld,\n", (long)((data_in[106] << 24) + (data_in[107] << 16) + (data_in[100] << 8) + data_in[101]));
+  printf("\"maxEraseCount\": %ld,\n", (long)((data_in[108] << 24) + (data_in[109] << 16) + (data_in[102] << 8) + data_in[103]));
 
-  printf("\"avgEraseCount\": %ld,\n", (long)((data_in[104]<<24)+(data_in[105]<<16)+(data_in[98]<<8)+data_in[99]));
-  printf("\"minEraseCount\": %ld,\n", (long)((data_in[106]<<24)+(data_in[107]<<16)+(data_in[100]<<8)+data_in[101]));
-  printf("\"maxEraseCount\": %ld,\n", (long)((data_in[108]<<24)+(data_in[109]<<16)+(data_in[102]<<8)+data_in[103]));
-
-  printf("\"powerUpCount\": %ld,\n", (long)((data_in[112]<<24)+(data_in[113]<<16)+(data_in[114]<<8)+data_in[115]));
-  printf("\"abnormalPowerOffCount\": %d,\n", (int)((data_in[128]<<8)+data_in[129]));
-  printf("\"totalRefreshCount\": %d,\n", (int)((data_in[160]<<8)+data_in[161]));
+  printf("\"powerUpCount\": %ld,\n", (long)((data_in[112] << 24) + (data_in[113] << 16) + (data_in[114] << 8) + data_in[115]));
+  printf("\"abnormalPowerOffCount\": %d,\n", (int)((data_in[128] << 8) + data_in[129]));
+  printf("\"totalRefreshCount\": %d,\n", (int)((data_in[160] << 8) + data_in[161]));
   printf("\"productMarker\": "
-           "[\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\","
-           "\"0x%02x\",\"0x%02x\"],\n",
-           data_in[176], data_in[177], data_in[178], data_in[179], data_in[180],
-           data_in[181], data_in[182], data_in[183]);
+         "[\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\","
+         "\"0x%02x\",\"0x%02x\"],\n",
+         data_in[176], data_in[177], data_in[178], data_in[179], data_in[180], data_in[181], data_in[182], data_in[183]);
   //  printf("\"badBlockCountPerDie2\": "
   //         "[\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\","
   //         "\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\",\"0x%02x\","
@@ -306,8 +297,9 @@ int main(int argc, const char *argv[]) {
   //         data_in[209], data_in[210], data_in[211], data_in[212], data_in[213],
   //         data_in[214], data_in[215]);
   // sum up to get later bad block count
-  sum=0;
-  for(i=184; i<216; i++) sum+=data_in[i];
+  sum = 0;
+  for (i = 184; i < 216; i++)
+    sum += data_in[i];
   printf("\"laterBadBlockCount\": %ld,\n", sum);
 
   close(fd);
